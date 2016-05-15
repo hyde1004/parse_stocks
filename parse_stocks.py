9436ff056021b6fb80bc17e6ea1e6458c1878779
이정구 <hyde1004@ijeong-guui-MacBook-Pro.local>
2016-05-15 14:08:18 +0900

    parse stocks

--- parse_stocks.py
+++ parse_stocks.py
@@ -1,2 +1,16 @@
 # http://paxnet.moneta.co.kr/stock/searchStock/searchStock.jsp?section=0
 # http://paxnet.moneta.co.kr/stock/searchStock/searchStock.jsp?section=1
+
+import bs4
+from urllib import parse
+import requests
+
+url = 'http://paxnet.moneta.co.kr/stock/searchStock/searchStock.jsp?section=0'
+response = requests.get(url)
+response.encoding='cp949'
+data = bs4.BeautifulSoup(response.text, 'html.parser')
+trs = data.find_all('tr')
+for tr in trs:
+	tds = tr.find_all('a')
+	for td in tds:
+		print(td.text)
